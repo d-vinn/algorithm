79e13f04e9af360615f5b8a4acb6ef9d5f585686
d-vinn <108311332+d-vinn@users.noreply.github.com>
2023-03-06 22:58:23 +0900

    [Bronze V] Title: A/B, Time: 116 ms, Memory: 13032 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		double result = A / B;
+		System.out.printf("%.9f", result);
+	}
+
+}
