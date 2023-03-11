9c4b3fb2b0473cef3b6539bc25c8a7ce056debf7
d-vinn <108311332+d-vinn@users.noreply.github.com>
2023-03-11 20:42:24 +0900

    [Bronze V] Title: 구구단, Time: 120 ms, Memory: 13072 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=1;i<=9;i++) {
+			System.out.printf("%d * %d = %d\n", N, i, N*i);
+		}
+	}
+
+}
